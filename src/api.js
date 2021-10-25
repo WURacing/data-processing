70ef2337585dcc4b5a5597f2850bdcf67b2a0c4d
bsiderowf <bsiderowf@gmail.com>
2021-10-25 15:10:01 -0500

    Track traces by name

--- src/api.js
+++ src/api.js
@@ -1,32 +1,40 @@
-// export interface DayMetadata {
-//     date: string,
-//     location?: string,
-//     timezone: string,
-//     start: string,
-//     end: string,
-//     intervals: {
-//       id: number,
-//       type?: string,
-//       description?: string,
-//       start: string,
-//       end: string
-//     }[],
-//     variables: {
-//       id: number,
-//       name: string,
-//       description?: string,
-//       units?: string
-//     }[]
-//   }
-
+/**
+ * @param {string} date
+ * @returns {Promise<{
+    date: string,
+    location?: string,
+    timezone: string,
+    start: string,
+    end: string,
+    intervals: {
+      id: number,
+      type?: string,
+      description?: string,
+      start: string,
+      end: string
+    }[],
+    variables: {
+      id: number,
+      name: string,
+      description?: string,
+      units?: string
+    }[]
+  }>}
+ */
 export async function getMetadata(date) {
-  // `https://data.washuracing.com/api/v2/testing/${date}`
-  return (await fetch('/metadata.json')).json();
+  const url = `https://data.washuracing.com/api/v2/testing/${date}`;
+  return (await fetch(url)).json();
 }
 
-// type DataResolution = '1ms' | '10ms' | '100ms' | '1s' | '10s' | '1m';
+/**
+ * @param {string} date
+ * @param {string} startTime
+ * @param {string} endTime
+ * @param {'1ms' | '10ms' | '100ms' | '1s' | '10s' | '1m'} resolution
+ * @returns {Promise<any[]>}
+ */
 export async function getData(date, startTime, endTime, resolution) {
-  // `https://data.washuracing.com/api/v2/testing/${date}/${startTime}/${endTime}/data?resolution=${resolution}`
-  const response = await fetch('/data.json');
+  const url = `https://data.washuracing.com/api/v2/testing/${date}/${startTime}/${endTime}/data?resolution=${resolution}`;
+  const response = await fetch(url);
   return response.json();
 }
