3e3c9df23367ad3eff01381fbba2d7445072435a
bsiderowf <bsiderowf@gmail.com>
2022-04-02 12:20:18 -0500

    Parse uploaded file with cantools

---
+++ dataviewer/can.py
@@ -0,0 +1,24 @@
+from csv import DictReader
+from typing import Any
+from cantools.database import load_file
+from flask import current_app
+
+EXTENDED_MASK = 0x1FFFFFFF
+
+def decode_csv(reader: DictReader) -> tuple[list[dict[str, float | str]], list[str]]:
+    can_file = current_app.config['CAN_FILE']
+    dbc = load_file(can_file)
+
+    field_names = set()
+    messages = []
+    
+    for row in reader:
+        id = int(row['id'], 16) & EXTENDED_MASK
+        data = bytes.fromhex(row['data'])
+        time = int(row['time'])
+
+        message = dbc.decode_message(id, data)
+        field_names |= set(message.keys())
+        message['time'] = time
+        messages.append(message)
+    return messages, ['time'] + list(field_names)
