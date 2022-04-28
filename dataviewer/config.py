4feebf006164fcc20599dcc91bf1f830c9b80b1a
Connor Monahan <cmonahan@wustl.edu>
2022-04-28 15:43:59 -0500

    Add waitress launcher

--- dataviewer/config.py
+++ dataviewer/config.py
@@ -9,13 +9,13 @@ class Config:
     """Configuration for the data viewer."""
 
     can_file: str = os.environ.get("CAN_FILE", "../VEHICLE.dbc")
-    port: str = os.environ.get("WAREHOUSE_POSTGRES_PORT", "5432")
+    port: str = os.environ.get("DATABASE_PORT", "5432")
     dialect: str = os.environ.get("DATABASE_DIALECT", "postgresql")
     driver: str = os.environ.get("DATABASE_DRIVER", "psycopg2")
     database: str = os.environ.get("DATABASE_NAME", "washuracing")
     username: str = os.environ["DATABASE_USER"]
     password: str = os.environ["DATABASE_PASSWORD"]
-    host: str = os.environ.get("WAREHOUSE_HOST", "localhost")
+    host: str = os.environ.get("DATABASE_HOST", "localhost")
 
     @property
     def database_uri(self) -> str:
---
+++ main.py
@@ -0,0 +1,10 @@
+import waitress
+from dataviewer.app import create_app, db
+
+
+app = create_app()
+
+with app.app_context():
+    db.create_all()
+
+waitress.serve(app, host="127.0.0.1", port=5000)
